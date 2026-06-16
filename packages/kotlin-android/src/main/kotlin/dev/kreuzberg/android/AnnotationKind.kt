bf66e77fef24307a7be9af2c9745a6373db6c89e
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-06-16 16:54:03 +0200

    Revert "style: post-generation format pass for v3.6.11 alef output"
    This reverts commit ae246ca93100ce6e51dbf6ba46a293f790807567.

--- packages/kotlin-android/src/main/kotlin/dev/kreuzberg/android/AnnotationKind.kt
+++ packages/kotlin-android/src/main/kotlin/dev/kreuzberg/android/AnnotationKind.kt
@@ -58,9 +58,7 @@ sealed class AnnotationKind {
     ) : AnnotationKind()
 }
 
-private class AnnotationKindDeserializer : com.fasterxml.jackson.databind.deser.std.StdDeserializer<AnnotationKind>(
-    AnnotationKind::class.java
-) {
+private class AnnotationKindDeserializer : com.fasterxml.jackson.databind.deser.std.StdDeserializer<AnnotationKind>(AnnotationKind::class.java) {
     @Suppress("LongMethod")
     override fun deserialize(
         parser: com.fasterxml.jackson.core.JsonParser,
@@ -81,10 +79,7 @@ private class AnnotationKindDeserializer : com.fasterxml.jackson.databind.deser.
             "highlight" -> AnnotationKind.Highlight
             "link" -> ctx.readTreeAsValue<AnnotationKind.Link>(payload, AnnotationKind.Link::class.java)
             else -> throw com.fasterxml.jackson.databind.exc.InvalidFormatException(
-                parser,
-                "Unknown AnnotationKind tag",
-                tag,
-                AnnotationKind::class.java,
+                parser, "Unknown AnnotationKind tag", tag, AnnotationKind::class.java,
             )
         }
     }
@@ -98,9 +93,7 @@ private class AnnotationKindSerializer : com.fasterxml.jackson.databind.ser.std.
         provider: com.fasterxml.jackson.databind.SerializerProvider,
     ) {
         @Suppress("UNCHECKED_CAST")
-        val mapper =
-            (gen.codec as? com.fasterxml.jackson.databind.ObjectMapper)
-                ?: com.fasterxml.jackson.databind.ObjectMapper().findAndRegisterModules()
+        val mapper = (gen.codec as? com.fasterxml.jackson.databind.ObjectMapper) ?: com.fasterxml.jackson.databind.ObjectMapper().findAndRegisterModules()
         val node: com.fasterxml.jackson.databind.node.ObjectNode = when (value) {
             is AnnotationKind.Bold -> {
                 val n = mapper.createObjectNode()
@@ -144,9 +137,7 @@ private class AnnotationKindSerializer : com.fasterxml.jackson.databind.ser.std.
             }
             is AnnotationKind.Link -> {
                 @Suppress("UNCHECKED_CAST")
-                val n = mapper.valueToTree<com.fasterxml.jackson.databind.node.ObjectNode>(
-                    value as AnnotationKind.Link
-                ) as com.fasterxml.jackson.databind.node.ObjectNode
+                val n = mapper.valueToTree<com.fasterxml.jackson.databind.node.ObjectNode>(value as AnnotationKind.Link) as com.fasterxml.jackson.databind.node.ObjectNode
                 n.put("annotation_type", "link")
                 n
             }
--- packages/php/src/HtmlToMarkdown.php
+++ packages/php/src/HtmlToMarkdown.php
@@ -23,9 +23,8 @@ final class HtmlToMarkdown
      * @throws \HtmlToMarkdown\HtmlToMarkdownException
      */
     public static function convert(
-        string $html,
-        ?ConversionOptions $options = null,
-    ): ConversionResult {
+string $html, ?ConversionOptions $options = null): ConversionResult
+    {
         return \HtmlToMarkdown\HtmlToMarkdownApi::convert($html, $options); // delegate to native extension class
     }
 }
